d0bf5a2e8eb8bcb2d036cbf8d926a0f2f0f90d12
Ime in Priimek <vas.email@email.com>
2024-09-19 00:33:17 +0200

    končno nekaj deluje

--- KONCNA_DATOTEKA.py
+++ KONCNA_DATOTEKA.py
@@ -7,17 +7,20 @@ import os
 
 # pomožna funkcija 1
 def pocisti_imena_smucisc(niz):
-    pocisceno_ime = re.sub(r'<span class=".*?">.*?</span>', '', niz)
-    pocisceno_ime = html.unescape(pocisceno_ime).strip() 
-    pocisceno_ime = re.sub(r'\s+', ' ', pocisceno_ime)  
-    pocisceno_ime = re.sub(r'\u200b', '', pocisceno_ime)
+    pocisceno_ime = re.sub(r'\s+', ' ', niz) 
+    pocisceno_ime = html.unescape(pocisceno_ime).strip()  
+    pocisceno_ime = re.sub(r'\u200b', '', pocisceno_ime)  
+    pocisceno_ime = re.sub(r'<.*?>', '', pocisceno_ime)
     return pocisceno_ime
+
 
 # pomožna funkcija 2
 def pocisti_celine_in_drzave(niz):
-    pocisceno_ime = html.unescape(niz).strip()
-    pocisceno_ime = re.sub(r'\s+', ' ', pocisceno_ime)  
+    pocisceno_ime = re.sub(r'\s*<span\s*class="closed-resort\s+\w+">\s*', '', niz)
     pocisceno_ime = re.sub(r'\u200b', '', pocisceno_ime)
+    pocisceno_ime = html.unescape(pocisceno_ime).strip()
+    pocisceno_ime = re.sub(r'\s+', ' ', pocisceno_ime)  
+
     return pocisceno_ime
 
 
@@ -27,7 +30,7 @@ def pocisti_celine_in_drzave(niz):
 vzorec_bloka = re.compile(
     r'<div class="panel panel-default resort-list-item resort-list-item-image--big"'  # Začetek bloka smučišča
     r'.*?'
-    r'<div class=".*?"><a class=".*?"\s*href=".*?">\s*Details\s*</a>\s*</div>\s*</div>\s*</div>',  # Konec bloka smučišča
+    r'<div\s*class=".*?">\s*<a\s+class=".*?"\s+href=".*?">\s*Details\s*</a>\s*</div>\s*</div>\s*</div>',  # Konec bloka smučišča
     flags=re.DOTALL
 )
 
@@ -35,19 +38,16 @@ vzorec_bloka = re.compile(
 # natančnejša obdelava 
 vzorec_smučišča = re.compile(
     # ime smučišča ter mesto, na katerem se nahaja po velikosti
-    r'<a class="h3"\s*href=".*?">\s*(?P<mesto_po_velikosti>\d+)\.\s+(?P<ime>.*?)</a>.*?'
+    r'<a\s+class="h3"\s+href=".*?">\s*(?P<mesto_po_velikosti>\d+)\.\s+(?P<ime>.*?)\s*</a>.*?'
     # celina in država
-    r'<div class="sub-breadcrumb">\s*<div\s*class="\s*sub-breadcrumb">\s*<a\s*href=".*?">(?P<celina>.*?)</a>\s*<a\s*href=".*?">(?P<drzava>.*?)</a>.*?'
+    r'<div\s+class="sub-breadcrumb">\s*<a\s+href=".*?">(?P<celina>.*?)</a>\s*(<a\s+href=".*?">\s*(?P<drzava>.*?)</a>)?.*?'
     # ocena smučišča
-    r'<div\s*class="\s*rating-list\s+js-star-ranking\s+stars-middle".*?data-rank="(?P<ocena>\d(\.\d)?)".*?'
-    # višinska razlika
-    r'<td>\s*<span>(?P<visinska_razlika>\d+(\.\d)?)\s+m</span>.*?'
-    # dolžina rdečih prog
-    r'<span\s*class="slopeinfoitem\s+red">\s*(?P<dolzina_rdecih_prog>\d+(\.\d)?)\s*km\s*</span>.*?'
-    # dolžina črnih prog
-    r'<span\s*class="slopeinfoitem\s+black">\s*(?P<dolzina_crnih_prog>\d+(\.\d)?)\skm\s*</span>.*?'
-    # število žičnic
-    r'<li>\s*(?P<stevilo_zicnic>\d+)\s*ski\s*lifts\s*</li>.*?',
+    r'<tr>.*?(<div class="rating-list js-star-ranking stars-middle".*?data-rank="(?P<ocena>\d(\.\d)?)".*?)?</tr>.*?'
+    # dolžina prog
+    r'<td>\s*<span\s*class="slopeinfoitem\s*'
+    r'active">(?P<skupna_dolzina>\d+(\.\d+)?)\skm</span>.*?(<span class="slopeinfoitem blue">(?P<dolzina_modrih>\d+(\.\d+)?)\skm</span>.*?)?'
+    r'(<span class="slopeinfoitem red">(?P<dolzina_rdecih>\d+(\.\d+)?)\skm</span>.*?)?'
+    r'(<span class="slopeinfoitem black">(?P<dolzina_crnih>\d+(\.\d+)?)\skm</span>.*?)?</td>',
     flags=re.DOTALL
 )
 
@@ -58,14 +58,15 @@ def izloci_podatke_smucisca(blok):
     smucisce = najdba.groupdict() # vrne slovar vseh vzorcev, poimenovanih zgoraj
     
     smucisce['mesto_po_velikosti'] = int(smucisce['mesto_po_velikosti'])
-    smucisce['ime'] = pocisti_celine_in_drzave(smucisce['ime'])
+    smucisce['ime'] = pocisti_imena_smucisc(smucisce['ime'])
     smucisce['celina'] = pocisti_celine_in_drzave(smucisce['celina'])
-    smucisce['ocena'] = float(smucisce['ocena'])
-    smucisce['visinska_razlika'] = int(smucisce['visinska_razlika'])
-    smucisce['stevilo_zicnic'] = int(smucisce['stevilo_zicnic']) 
-    smucisce['dolzina_rdecih_prog'] = float(smucisce['dolzina_rdecih_prog'])
-    smucisce['dolzina_crnih_prog'] = float(smucisce['dolzina_crnih_prog'])
-    
+    smucisce['drzava'] = pocisti_celine_in_drzave(smucisce['drzava']) if smucisce['drzava'] not in (None, '') else 'Country is not provided.'
+    smucisce['ocena'] = float(smucisce['ocena']) if smucisce['ocena'] not in (None, '') else 'There is no rating.'
+    smucisce['skupna_dolzina'] = float(smucisce['skupna_dolzina'])
+    smucisce['dolzina_modrih'] = float(smucisce['dolzina_modrih']) if smucisce['dolzina_modrih'] not in (None, '') else 'Not known.'
+    smucisce['dolzina_rdecih'] = float(smucisce['dolzina_rdecih']) if smucisce['dolzina_rdecih'] not in (None, '') else 'Not known.'
+    smucisce['dolzina_crnih'] = float(smucisce['dolzina_crnih']) if smucisce['dolzina_crnih'] not in (None, '') else 'Not known.'
+
     return smucisce
 
 
@@ -83,7 +84,7 @@ def izloci_vsa_smucisca(vsebina):
 
 
 seznam_smucisc = []
-for i in range(1, 3):
+for i in range(1, 17):
     with open(os.path.join('smucisca', f'smucisca{i}.html'), 'r', encoding='utf-8') as f:
         stran = f.read()
     smucisca_iz_datoteke = izloci_vsa_smucisca(stran)
