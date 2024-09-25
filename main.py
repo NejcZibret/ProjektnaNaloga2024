552b3935744db4c4957037e3f6133cda6a381cc7
Ime in Priimek <vas.email@email.com>
2024-09-26 00:19:49 +0200

    n

--- main.py
+++ main.py
@@ -129,8 +129,10 @@ def izloci_podatke_smucisca(blok):
     smucisce['ime'] = pocisti_imena_smucisc(smucisce['ime'])
     smucisce['celina'] = pocisti_celine_in_drzave(smucisce['celina'])
     if smucisce['celina'] == 'Russia':
-        smucisce['drzava'] = 'Russia'
-        smucisce['celina'] = 'Europe/Asia'
+        if smucisce['drzava'] in ['Ural Federal District', 'Southern Russia', 'Northwest Russia', 'North Caucasus', 'Volga Federal District', 'Central Russia']:
+            smucisce['celina'] = 'Europe'
+        else:
+            smucisce['celina'] = 'Asia'
     smucisce['drzava'] = pocisti_celine_in_drzave(smucisce['drzava']) if smucisce['drzava'] not in (None, '') else 'no data'
     smucisce['ocena'] = float(smucisce['ocena']) if smucisce['ocena'] not in (None, '') else np.nan
     smucisce['visinska_razlika'] = float(smucisce['visinska_razlika']) if smucisce['visinska_razlika'] not in (None, '') else np.nan
